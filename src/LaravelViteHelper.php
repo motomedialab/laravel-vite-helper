d5598057bd650b7ecd31e90600756739334c7e2c
Chris Page <sirhc.page@gmail.com>
2023-04-20 08:46:28 +0100

    Reversed parameter check on if statement
    Reversed parameter check on if statement to perform the easiest logical check first.

--- src/LaravelViteHelper.php
+++ src/LaravelViteHelper.php
@@ -17,15 +17,15 @@ class LaravelViteHelper
      * @param  string  $resourcePath
      * @param  string  $buildDirectory
      * @param  bool    $relative  Whether to return a relative path or absolute path
-     * @param  bool    $hotServer  Force enable/disble the hot server
+     * @param  bool    $hotServer  Force enable/disable the hot server
      * @return string
      *
      * @throws \Exception
      */
     public function resourceUrl($resourcePath, $buildDirectory = 'build', $relative = false, $hotServer = true)
     {
-        if ($hotServer = $this->hotServer() &&  $hotServer = true) {
-            return "$hotServer/$resourcePath";
+        if ($hotServer && $server = $this->hotServer()) {
+            return "$server/$resourcePath";
         }
 
         $manifest = $this->manifestContents($buildDirectory);
